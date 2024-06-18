a1ffe4e3b7be507348f365e11a4111ad280642ec
Omar Raafat <omar.raafat@bit68.com>
2024-06-19 02:07:03 +0300

    Reordering modules and their contents

--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.mixins import (
 from django.urls import reverse_lazy
 from django.forms.models import modelform_factory
 from django.apps import apps
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
@@ -186,3 +187,31 @@ class ModuleContentListView(TemplateResponseMixin, View):
             course__owner=request.user,
         )
         return self.render_to_response({"module": module})
+
+
+class ModuleOrderView(
+    CsrfExemptMixin,
+    JsonRequestResponseMixin,
+    View,
+):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(
+                id=id,
+                course__owner=request.user,
+            ).update(order=order)
+        return self.render_json_response({"saved": "OK"})
+
+
+class ContentOrderView(
+    CsrfExemptMixin,
+    JsonRequestResponseMixin,
+    View,
+):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(
+                id=id,
+                module__course__owner=request.user,
+            ).update(order=order)
+        return self.render_json_response({"saved": "OK"})
