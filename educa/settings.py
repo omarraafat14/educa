20330260ace41abb4a5c4bb081d2c9a8f5eec7f2
Omar Raafat <omar.raafat@bit68.com>
2024-06-21 17:51:01 +0300

    Accessing the course contents

--- educa/settings.py
+++ educa/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.urls import reverse_lazy
+
+LOGIN_REDIRECT_URL = reverse_lazy("student_course_list")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
