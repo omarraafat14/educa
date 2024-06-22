d0c401e0a11e369a2590fb771c1898a6e6b7beac
Omar Raafat <omar.raafat@bit68.com>
2024-06-22 18:02:57 +0300

    Adding additional actions to ViewSets

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -20,10 +20,10 @@ urlpatterns = [
         views.SubjectDetailView.as_view(),
         name="subject_detail",
     ),
-    path(
-        "courses/<pk>/enroll/",
-        views.CourseEnrollView.as_view(),
-        name="course_enroll",
-    ),
+    # path(
+    #     "courses/<pk>/enroll/",
+    #     views.CourseEnrollView.as_view(),
+    #     name="course_enroll",
+    # ),
     path("", include(router.urls)),
 ]
