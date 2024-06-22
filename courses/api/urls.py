938d469508c51c6a2e64871363aa5eb892cb15df
Omar Raafat <omar.raafat@bit68.com>
2024-06-22 17:57:52 +0300

    Creating ViewSets and routers

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+
+router = routers.DefaultRouter()
+router.register("courses", views.CourseViewSet)
 
 app_name = "courses"
 
@@ -21,4 +25,5 @@ urlpatterns = [
         views.CourseEnrollView.as_view(),
         name="course_enroll",
     ),
+    path("", include(router.urls)),
 ]
