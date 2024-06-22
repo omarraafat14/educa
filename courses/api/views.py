e2974b2dd0a109cab981639b32c31166cb2fe76d
Omar Raafat <omar.raafat@bit68.com>
2024-06-22 18:40:01 +0300

    Serializing course contents

--- courses/api/views.py
+++ courses/api/views.py
@@ -8,6 +8,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseWithContentSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -44,3 +46,12 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({"enrolled": True})
+
+    @action(
+        detail=True,
+        methods=["get"],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
