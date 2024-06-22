e5b687cb4af21d7f38a707665fc2289def7282f5
Omar Raafat <omar.raafat@bit68.com>
2024-06-22 15:54:25 +0300

    Using the Redis cache backend

--- educa/settings.py
+++ educa/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-        "LOCATION": "127.0.0.1:11211",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
     }
 }
