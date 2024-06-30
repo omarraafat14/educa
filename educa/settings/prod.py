e09b084813583ffa9a2a8fc09c2acfcccea750d1
Omar Raafat <omar.raafat@bit68.com>
2024-06-30 14:33:26 +0300

    Configuring the PostgreSQL service

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -1,3 +1,4 @@
+from decouple import config
 from .base import *
 
 DEBUG = False
@@ -5,4 +6,13 @@ ADMINS = [
     ("Omar Raafat", "omar.raafat@mydomain.com"),
 ]
 ALLOWED_HOSTS = ["*"]
-DATABASES = {"default": {}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
+    }
+}
